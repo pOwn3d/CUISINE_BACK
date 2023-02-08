2786b0d2fc4c7f320ef0bb8b6e3a39a61603a19d
Lopez <46645541+pOwn3d@users.noreply.github.com>
2023-02-08 14:01:51 +0100

    update bdd

--- config/database.js
+++ config/database.js
@@ -2,11 +2,11 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST', 'containers-us-west-183.railway.app'),
-      port: env.int('DATABASE_PORT', 6550),
+      host: env('DATABASE_HOST', 'containers-us-west-135.railway.app'),
+      port: env.int('DATABASE_PORT', 6321),
       database: env('DATABASE_NAME', 'railway'),
       user: env('DATABASE_USERNAME', 'postgres'),
-      password: env('DATABASE_PASSWORD', '1vid8MLmAhC59vc0TixB'),
+      password: env('DATABASE_PASSWORD', 'K25gwuAXIvYn0yjCGWP8'),
       ssl: {
         rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
       },
