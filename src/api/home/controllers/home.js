f2d606d0b7cd118629e52b93e2156c6a9b18c0f0
pown3d <compt3@gmail.com>
2023-02-16 22:03:56 +0100

    add cloudinary

--- src/api/home/controllers/home.js
+++ src/api/home/controllers/home.js
@@ -1,9 +1,9 @@
 'use strict';
 
 /**
- * test controller
+ * home controller
  */
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::test.test');
+module.exports = createCoreController('api::home.home');
